4ca5f64e627d148782e1a329d0ca2e7a251cac7d
Bill Li <billxinli@users.noreply.github.com>
2020-11-11 22:41:00 -0500

    fix(headers): allow headers to be parsed from OAS json
    * fix(header): adding in header parsing
    
    * fix(header): tests for adding in header parsing
    
    * fix(header): simplify parameters

--- test/fixtures/fastify.js
+++ test/fixtures/fastify.js
@@ -2,7 +2,11 @@ module.exports = [{
   url: '/pets',
   method: 'GET',
   schema: {
-    params: undefined,
+    params: {
+      type: 'object',
+      required: [],
+      properties: {}
+    },
     query: {
       type: 'object',
       required: [],
@@ -25,14 +29,25 @@ module.exports = [{
   url: '/pets',
   method: 'POST',
   schema: {
-    params: undefined,
-    query: undefined,
+    params: {
+      type: 'object',
+      required: [],
+      properties: {}
+    },
+    query: {
+      type: 'object',
+      required: [],
+      properties: {}
+    },
     headers: {
       type: 'object',
-      required: ['content-type'],
+      required: ['X-API-SECRET', 'content-type'],
       properties: {
         'content-type': {
           const: 'x-www-form-urlencoded'
+        },
+        'X-API-SECRET': {
+          type: 'string'
         }
       }
     },
@@ -60,7 +75,11 @@ module.exports = [{
   url: '/pets/:petId',
   method: 'GET',
   schema: {
-    query: undefined,
+    query: {
+      type: 'object',
+      required: [],
+      properties: {}
+    },
     params: {
       type: 'object',
       required: ['petId'],
