19c3f37b51e52770012e66a06c7920519be19ff7
Bill Li <me@billxinli.com>
2021-02-18 14:56:54 -0500

    feat(security): adding validation to headers and query for security

--- test/index.js
+++ test/index.js
@@ -4,6 +4,9 @@ const { test } = require('tap')
 const openAPISpec = require('./fixtures/openapi.json')
 const fastifySpec = require('./fixtures/fastify')
 
+const openAPISpecSecurity = require('./fixtures/openapi-with-security.json')
+const fastifySpecSecurity = require('./fixtures/fastify-with-security')
+
 test('oas-to-fastify', assert => {
   assert.plan(2)
 
@@ -27,4 +30,29 @@ test('oas-to-fastify', assert => {
 
   assert.match(result, fastifySpec)
   assert.strictSame(openAPISpec, schema)
+})
+
+test('oas-to-fastify with security', assert => {
+  assert.plan(2)
+
+  let schema
+  const result = []
+
+  // fastify mock
+  const fastify = {
+    addSchema: (spec) => (schema = spec),
+    route: (route) => result.push(route)
+  }
+
+  // handler mock
+  const handler = {
+    listPets: 'listPets',
+    createPets: 'createPets',
+    showPetById: 'showPetById'
+  }
+
+  plugin(fastify, { spec: openAPISpecSecurity, handler }, () => {})
+
+  assert.match(result, fastifySpecSecurity)
+  assert.strictSame(openAPISpecSecurity, schema)
 })
